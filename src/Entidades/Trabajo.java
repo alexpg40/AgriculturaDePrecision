a8f906f2bd221691c7f0713ce5d0cfeb5fe5a4ff
DAW207 <--global>
2021-10-04 18:25:49 +0200

    metodo eligirTarea, ejecutarTrabajo, crearTrabajo creados

--- src/Entidades/Trabajo.java
+++ src/Entidades/Trabajo.java
@@ -5,6 +5,8 @@
  */
 package Entidades;
 
+import java.util.Scanner;
+
 /**
  *
  * @author DAW207
@@ -59,6 +61,44 @@ public class Trabajo {
         this.estado = estado;
     }
     
+    public static String elegirTarea(){
+        System.out.println("Introduce 1 para Abonar o 2 para Fumigar");
+        Scanner in = new Scanner(System.in);
+        int opcion = in.nextInt();
+        switch(opcion){
+            case 1:
+                return "Abonar";
+            default:
+                return "Fumigar";
+        }
+    }
     
+    public boolean ejecutarTrabajo(Lista<Parcela> parcelas){
+        Nodo<Parcela> aux = parcelas.getNodoPrimero();
+        while(aux != null){
+            if(aux.getInfo().getIdParcela() == this.idParcela){
+                System.out.println("Empezando la tarea con id " + this.idTrabajo);
+                Nodo<Punto> aux2 = aux.getInfo().getListaPuntos().getNodoPrimero();
+                while(aux2 != null){
+                    System.out.println("Estoy en el " + aux2.getInfo());
+                    aux2 = aux2.getSig();
+                }
+                this.estado = "Terminada";
+                System.out.println("Tarea terminada!");
+            }
+            aux = aux.getSig();
+        }
+    return false;}
+    
+    public static Trabajo crearTrabajo(){
+        Trabajo ret = new Trabajo();
+        System.out.println("Introduce el idTrabajo: ");
+        Scanner in = new Scanner(System.in);
+        ret.setIdTrabajo(in.nextInt());
+        System.out.println("Introduce la idParcela: ");
+        ret.setIdParcela(in.nextInt());
+        ret.setTipoTarea(Trabajo.elegirTarea());
+        ret.setEstado("En espera");
+    return ret;}
     
 }
