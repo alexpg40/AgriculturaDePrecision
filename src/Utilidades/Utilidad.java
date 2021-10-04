613ee31ece196725da7e9648ab98d6d6b2c3a5d5
DAW207 <--global>
2021-10-04 19:34:02 +0200

    primer commit de Utilidad

---
+++ src/Utilidades/Utilidad.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilidades;
+
+import Entidades.*;
+
+public class Utilidad {
+    
+    public static <T> void mostrar(Lista<T> lista){
+        Nodo aux = lista.getNodoPrimero();
+        while(aux != null){
+            T t = (T) aux.getInfo();
+            System.out.println(t);
+            aux = aux.getSig();
+        }
+    }
+    
+}
