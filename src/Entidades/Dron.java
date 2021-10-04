1d5e62de0dea128fff64e0f150bf97a03d08f39f
DAW207 <--global>
2021-10-04 19:37:13 +0200

    añadido regristarDron

--- src/Entidades/Dron.java
+++ src/Entidades/Dron.java
@@ -5,6 +5,8 @@
  */
 package Entidades;
 
+import java.util.Scanner;
+
 /**
  *
  * @author DAW207
@@ -13,4 +15,38 @@ public class Dron {
     
     private int idDron;
     private int idAgricultor;
+    
+    public Dron(){
+        
+    }
+    
+    public Dron(int idDron, int idAgricultor){
+        this.idDron = idDron;
+        this.idAgricultor = idAgricultor;
+    }
+
+    public int getIdDron() {
+        return idDron;
+    }
+
+    public void setIdDron(int idDron) {
+        this.idDron = idDron;
+    }
+
+    public int getIdAgricultor() {
+        return idAgricultor;
+    }
+
+    public void setIdAgricultor(int idAgricultor) {
+        this.idAgricultor = idAgricultor;
+    }
+    
+    public static Dron registarDron(Agricultor a){
+        Dron ret = new Dron();
+        ret.setIdAgricultor(a.getIdAgricultor());
+        Scanner in = new Scanner(System.in);
+        System.out.println("Introduce la id del dron: ");
+        ret.setIdDron(in.nextInt());
+    return ret;}
+    
 }
